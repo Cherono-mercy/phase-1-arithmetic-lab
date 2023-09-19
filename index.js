4ff2aa4128caa2451fa8fce00731771d2378f6fc
Mercy Cherono <cheronom142@gmail.com>
2023-09-19 15:54:52 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Multiplication operator
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Generating random number > 0
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Remainder operator
+let num3 = 52;
+let num4 = 6;
+const mod = num3 % num4;
+
+//Math.max method
+const max = Math.max(5, 2, 8, 15, 20);
